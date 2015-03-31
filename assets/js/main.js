8174e02fb3407d4b13fe9dae40cfda76bd628425
JamesWClark <jwclark@rockhursths.edu>
2015-03-30 23:56:34 -0500

    add a pages branch

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+﻿//mostly copied from http://www.html5rocks.com/en/tutorials/file/dndfiles/
+
+// Check for the various File API support.
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+	console.log('Great success! All the File APIs are supported.');
+} else {
+	alert('The File APIs are not fully supported in this browser.');
+}

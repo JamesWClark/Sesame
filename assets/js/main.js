4555afef354273fcfa19a7c122d5e73c929f6f9e
jwclark <jwclark@rockhursths.edu>
2015-04-01 10:30:47 -0500

    add multiple document processing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
 ﻿/**
 
 JW Clark
-started at  http://www.html5rocks.com/en/tutorials/file/dndfiles/
+
 load XML from file upload, parse it
+
+started at  http://www.html5rocks.com/en/tutorials/file/dndfiles/
+learned a closure - http://stackoverflow.com/questions/12546775/get-filename-after-filereader-asynchronously-loaded-a-file
 
 */
 
@@ -13,17 +16,35 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 	alert('The File APIs are not fully supported in this browser.');
 }
 
-
-var fileReader;
+var totalDocumentsProcessed = 0;
+var totalDocumentCount = 0;
 var totalWordCount = 0;
 var totalLemmaCount = 0;
-var wordNotCounted = [];
+var collectDocuments = {};
 var collectLemmas = {};
 var collectWords = {};
+var wordNotCounted = [];
 
-/**
-* Throws 
-*/
+function reset() {
+	totalDocumentsProcessed = 0;
+	totalDocumentCount = 0;
+	totalWordCount = 0;
+	totalLemmaCount = 0;
+	collectDocuments = {};
+	collectLemmas = {};
+	collectWords = {};
+	wordNotCounted = [];
+	$('output').html('');
+}
+
+function tf() {
+
+}
+
+function idf() {
+
+}
+
 function SentimentFormatException(sentiment) {
 	this.value = sentiment;
 	this.message = "Unhandled sentiment expression";
@@ -71,70 +92,88 @@ function incrementWordCounts(word) {
 }
 
 function parseCoreNLPXML() {
-	var parser = new DOMParser();
-	var xml = parser.parseFromString(fileReader.result, "application/xml");
-	var sentences = xml.getElementsByTagName('sentences')[0].children;
-	var totalSentimentScore = 0;
 
-	//foreach sentence
-	for (var i = 0; i < sentences.length; i++) {
-		var sentimentScore = parseInt(sentences[i].attributes[1].nodeValue);
-		var sentimentLabel = sentences[i].attributes[2].nodeValue;
-		var tokens = sentences[i].children[0].children;
+	//foreach document
+	for (var fileName in collectDocuments) {
+		if (collectDocuments.hasOwnProperty(fileName)) {
 
-		//foreach token
-		var tokenSentence = '';
-		for (var k = 0; k < tokens.length; k++) {
-			var iWord = 0;
-			var iLemma = 1;
-			var iPos = 4;
-			var iNer = 5;
-			var iSpeaker = 6;
-			var word = tokens[k].children[iWord].textContent;
-			var lemma = tokens[k].children[iLemma].textContent;
-			incrementWordCounts(word);
-			incrementLemmaCounts(lemma);
-			tokenSentence += word + ' ';
+			var parser = new DOMParser();
+			var xml = parser.parseFromString(collectDocuments[fileName], "application/xml");
+			var sentences = xml.getElementsByTagName('sentences')[0].children;
+			var totalSentimentScore = 0;
+			var html = '';
+
+			//foreach sentence
+			for (var i = 0; i < sentences.length; i++) {
+				var sentimentScore = parseInt(sentences[i].attributes[1].nodeValue);
+				var sentimentLabel = sentences[i].attributes[2].nodeValue;
+				var tokens = sentences[i].children[0].children;
+
+				//foreach token
+				var tokenSentence = '';
+				for (var k = 0; k < tokens.length; k++) {
+					var iWord = 0;
+					var iLemma = 1;
+					var iPos = 4;
+					var iNer = 5;
+					var iSpeaker = 6;
+					var word = tokens[k].children[iWord].textContent;
+					var lemma = tokens[k].children[iLemma].textContent;
+					incrementWordCounts(word);
+					incrementLemmaCounts(lemma);
+					tokenSentence += word + ' ';
+				}
+				totalSentimentScore += evaluateSentimentLabelScore(sentimentLabel);
+
+				html += '<br><div>' + (i + 1) + ': ' + sentimentLabel + '</div><div>' + tokenSentence + '</div>';
+			}
+			var containerDocument = '<div id="' + fileName + '">' + html + '</div>';
+			$('#totalSentimentScore').html('<b>Total Sentiment Score:</b> ' + totalSentimentScore);
+			$('#totalWordCount').html('<b>Word Count:</b> ' + totalWordCount);
+			$('#totalLemmaCount').html('<b>Lemma Count:</b> ' + totalLemmaCount);
+			$('#totalSentences').html('<b>Sentences:</b> ' + sentences.length);
+			$('#container-documents').append(containerDocument);
+
+			//console.log(sentences);
+			//console.log(wordNotCounted);
 		}
-		totalSentimentScore += evaluateSentimentLabelScore(sentimentLabel);
-
-		$('#list').append('<br><div>' + (i + 1) + ': ' + sentimentLabel + '</div><div>' + tokenSentence + '</div>');
 	}
-	$('#totalSentimentScore').html('<b>Total Sentiment Score:</b> ' + totalSentimentScore);
-	$('#totalWordCount').html('<b>Word Count:</b> ' + totalWordCount);
-	$('#totalLemmaCount').html('<b>Lemma Count:</b> ' + totalLemmaCount);
-	$('#totalSentences').html('<b>Sentences:</b> ' + sentences.length);
-	
 	var lemmaList = $('#lemma-frequency');
-
-	Object.keys(collectLemmas).sort(function (a, b) { return collectLemmas[a] - collectLemmas[b] });
-
 	for (var key in collectLemmas) {
 		if (collectLemmas.hasOwnProperty(key)) {
-			$(lemmaList).append('<li>' + key + ': ' + (collectLemmas[key]/totalLemmaCount).toFixed(5) + '</li>')
+			$(lemmaList).append('<li>' + key + ': ' + (collectLemmas[key] / totalLemmaCount).toFixed(5) + '</li>')
 		}
 	}
 
 	var wordList = $('#word-frequency');
-
-	Object.keys(collectWords).sort(function (a, b) { return collectWords[a] - collectWords[b] });
-
 	for (var key in collectWords) {
 		if (collectWords.hasOwnProperty(key)) {
-			$(wordList).append('<li>' + key + ': ' + (collectWords[key]/totalWordCount).toFixed(5) + '</li>')
+			$(wordList).append('<li>' + key + ': ' + (collectWords[key] / totalWordCount).toFixed(5) + '</li>')
 		}
 	}
-	console.log(sentences);
-	console.log(wordNotCounted);
+	$('#display').show();
 }
 
-function handleFileSelect(evt) {
-	var files = evt.target.files; // FileList object
+
+function handleFileSelect(event) {
+	reset();
+	var files = event.target.files; // FileList object
+	totalDocumentCount = files.length;
 	for (var i = 0, f; f = files[i]; i++) {
-		fileReader = new FileReader();
+		var fileReader = new FileReader();
+		fileReader.onloadend = (function (file) {
+			return function (evt) {
+				collectDocument(evt, file)
+			}
+		})(f);
 		fileReader.readAsText(f);
-		fileReader.onloadend = parseCoreNLPXML;
 	}
+}
+
+function collectDocument(evt, file) {
+	collectDocuments[file.name] = evt.target.result;
+	if (++totalDocumentsProcessed === totalDocumentCount)
+		parseCoreNLPXML();
 }
 
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
